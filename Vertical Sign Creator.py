74a2d21e833eca58783e55d267e66b34dd2f2f6a
Jordan Gudim <137233322+Jpgudim@users.noreply.github.com>
2023-06-25 17:55:21 -0500

    Add files via upload

---
+++ Vertical Sign Creator.py
@@ -0,0 +1,12 @@
+word = input("Type a word that you want to make vertical! ")
+
+print ()
+print ("   _____")
+print ("---|   |")
+
+for letter in word:
+    print ("   | " + letter + " |")
+
+print ("---|___|")
+print ()
+
